912b6589b19d2691ef502f19600608287eba79e0
Shubham Srivastava <cashivamsms@gmail.com>
2021-05-18 12:18:39 +0530

    adding

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require('dotenv').config({path: "./config.env"})
 const express = require('express');
 const connectDB = require('./config/db');
 
@@ -23,7 +22,3 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => console.log(`Server running on ${PORT}`))
 
 
-process.on("unhandledRejection", (err, promise) => {
-    console.log(`Logged Error: ${err}`);
-    server.close(() => process.exit(1));
-});
