6b21dd8f00a745ae088c2a42f5aec9821a483b6b
Shubham Srivastava <cashivamsms@gmail.com>
2021-05-20 11:53:57 +0530

    adding

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/api/user', require('./routes/user'));
 
 app.use('/api/recruiter', require('./routes/recruiter'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8900;
 
 const server = app.listen(PORT, () => console.log(`Server running on ${PORT}`))
 
